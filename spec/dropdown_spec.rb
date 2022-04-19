2500892409b6fbf84a12633b2695131468657ad2
Nathanael Ferreira Santana <nathanaelotaku28@gmail.com>
2022-04-18 22:58:27 -0300

    Teste com select simples, pesquisa e seleção randomica

---
+++ spec/dropdown_spec.rb
@@ -0,0 +1,25 @@
+
+
+describe 'Caixa de seleção', :dropdown do
+
+    it 'item especifico simples' do
+        visit 'https://training-wheels-protocol.herokuapp.com/dropdown'
+        select('Loki', from: 'dropdown')
+        sleep 3 
+    end
+
+    it 'item especifico com o find' do 
+        visit 'https://training-wheels-protocol.herokuapp.com/dropdown'
+        drop = find('.avenger-list')
+        drop.find('option', text: 'Scott Lang').select_option
+        sleep 3
+    end
+
+    it 'qualquer item', :sample do
+        visit 'https://training-wheels-protocol.herokuapp.com/dropdown'
+        drop = find('.avenger-list')
+        drop.all('option').sample.select_option
+        sleep 3
+    end
+
+end
