87cb080e5eea563f0489c939b639b3f488519fc0
Nathanael Ferreira Santana <nathanaelotaku28@gmail.com>
2022-04-22 15:25:52 -0300

    Adicionando teste de login com campo randomico, caso com o ruby

---
+++ spec/login_form_spec.rb
@@ -0,0 +1,37 @@
+
+
+describe 'login_Forms', :login_form  do
+    
+    it 'login com sucesso' do
+        visit 'https://training-wheels-protocol.herokuapp.com/login'
+
+        fill_in 'userId', with: 'stark'
+        fill_in 'password', with: 'jarvis!'
+        click_button 'Login'
+        expect(find('#flash').visible?).to be true
+        expect(find('#flash').text).to include 'Olá, Tony Stark. Você acessou a área logada!'
+        expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
+    end
+
+    it 'login com senha incorreta' do
+        visit 'https://training-wheels-protocol.herokuapp.com/login'
+
+        fill_in 'userId', with: 'stark'
+        fill_in 'password', with: 'xpto568'
+        
+        click_button 'Login'        
+        
+        expect(find('#flash')).to have_content 'Senha é invalida!'
+    end
+
+    it 'login com usuário não cadastrado' do
+        visit 'https://training-wheels-protocol.herokuapp.com/login'
+
+        fill_in 'userId', with: 'hulk'
+        fill_in 'password', with: 'xpto568'
+        
+        click_button 'Login'        
+        
+        expect(find('#flash')).to have_content 'O usuário informado não está cadastrado!'
+    end
+end
