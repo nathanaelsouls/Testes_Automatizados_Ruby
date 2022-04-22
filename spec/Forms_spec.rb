b1653336f008e359ffe2735d21736602e601150b
Nathanael Ferreira Santana <nathanaelotaku28@gmail.com>
2022-04-22 13:03:16 -0300

    Melhorando a busca de campos com o Regex

--- spec/Forms_spec.rb
+++ spec/Forms_spec.rb
@@ -1,21 +1,16 @@
 
 
 describe 'Forms'  do 
-
+    
     it 'login com sucesso', :login do
-        
+
         visit 'https://training-wheels-protocol.herokuapp.com/login'
-        
+
         fill_in 'userId', with: 'stark'
         fill_in 'password', with: 'jarvis!'
-
-        click_button 'Login'
-
-        expect(find('#flash').visible?).to be true
-
-        expect(find('#flash').text).to include 'Olá, Tony Stark. Você acessou a área logada!'
-
-        expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
-
-    end
+            click_button 'Login'
+            expect(find('#flash').visible?).to be true
+            expect(find('#flash').text).to include 'Olá, Tony Stark. Você acessou a área logada!'
+            expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
+    end    
 end
--- spec/hovers_spec.rb
+++ spec/hovers_spec.rb
@@ -5,21 +5,21 @@ describe 'Mouse Hover', :hovers do
     end
 
     it 'quando passo o mouse sobre o balde' do
-        card = find('img[alt=Blade]')
+        card = find('img[alt*=Blade]')#contem
         card.hover
 
         expect(page).to have_content 'Nome: Blade'
     end
 
     it 'quando passo o mouse sobre o pantera negra' do
-        card = find('img[alt="Pantera Negra"]')
+        card = find('img[alt^=Pantera]')#inicia
         card.hover
 
         expect(page).to have_content 'Nome: Pantera Negra'
     end
 
     it 'quando passo o mouse sobre o homem aranha' do
-        card = find('img[alt="Homem Aranha"]')
+        card = find('img[alt$=Aranha]')#termina
         card.hover
 
         expect(page).to have_content 'Nome: Homem Aranha'
