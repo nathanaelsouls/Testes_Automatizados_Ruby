99efeb218a6d6bd9d125fce1ece1f6ae17156069
Nathanael Ferreira Santana <nathanaelotaku28@gmail.com>
2022-04-22 13:57:00 -0300

    adicionando teste para drag and drop e iniciando teste para simulação com o teclado

---
+++ spec/key_spec.rb
@@ -0,0 +1,13 @@
+
+
+describe 'Teclado', :key do
+
+    before(:each) do
+        visti 'https://training-wheels-protocol.herokuapp.com/key_presses'
+    end
+
+    it 'enviando teclas' do
+        
+    end
+
+end
