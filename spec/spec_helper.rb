00c3fa7a6ccbf130ad858474245e27e637135a4b
Nathanael Ferreira Santana <nathanaelotaku28@gmail.com>
2022-04-23 19:56:39 -0300

    Adicionando configuração headless para reprodução dos testes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,17 +17,17 @@ RSpec.configure do |config|
   config.include Capybara::DSL
 
   config.before(:example) do
-    page.current_window.resize_to(1280,800)
+    page.current_window.resize_to(1920,1080)
   end
 
   config.after(:example) do |e|      
     nome = e.description.gsub(/[^A-Za-z0-9 ]/, '').tr(' ', '_')
-    page.save_screenshot('log/' + nome + '.png') if e.exception
+    page.save_screenshot('log/' + nome + '.png') #if e.exception
   end
 end
 
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome
+  config.default_driver = :selenium_chrome_headless
   config.default_max_wait_time = 5
   config.app_host = 'https://training-wheels-protocol.herokuapp.com'
 end
