83e6493abb1f73959c217542f4c4406215d497bd
Nathanael Ferreira Santana <nathanaelotaku28@gmail.com>
2022-04-23 17:48:08 -0300

    Adicionando teste para validar Dados em Tabelas

---
+++ spec/tabelas_spec.rb
@@ -0,0 +1,50 @@
+describe 'Tabelas', :tabs do
+
+    before(:each)do
+        visit 'https://training-wheels-protocol.herokuapp.com/tables'
+    end
+
+    #it 'pega o tonu stark na lista' do
+    #    expect(page).to have_content 'Robert Downey Jr'
+    #end
+
+    it 'deve exibir o salario do stark' do
+        atores = all('table tbody tr')
+        stark = atores.detect { |ator| ator.text.include?('Robert Downey Jr')}
+        expect(stark.text).to include '10.000.000'
+    end
+
+    it 'deve exibir o salario do vin diesel' do
+        diesel = find('table tbody tr', text: '@vindiesel')        
+        expect(diesel).to have_content '10.000.000'
+    end
+
+    it 'deve exibir o filme velozes' do
+        diesel = find('table tbody tr', text: '@vindiesel')
+        movie = diesel.all('td')[2].text
+        expect(movie).to eql 'Velozes e Furiosos'
+    end
+
+    it 'deve exibir o insta do Chris Evans' do
+        evans = find('table tbody tr', text: 'Chris Evans')
+        insta = evans.all('td')[4].text
+        expect(insta).to eql '@teamcevans'
+    end
+
+    it 'deve selecionar Chris Prat para remoção' do
+        prat = find('table tbody tr', text: 'Chris Prat')
+        prat.find('a', text: 'delete').click
+
+        msg = page.driver.browser.switch_to.alert.text
+        expect(msg).to eql 'Chris Pratt foi selecionado para remoção!'
+    end
+
+    it 'deve selecionar Chris Prat para edição' do
+        prat = find('table tbody tr', text: 'Chris Prat')
+        prat.find('a', text: 'edit').click
+
+        msg = page.driver.browser.switch_to.alert.text
+        expect(msg).to eql 'Chris Pratt foi selecionado para edição!'
+    end
+
+end
